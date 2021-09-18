b3ca0a37027fc8f26e18a6c21d4e6f2666281336
macan0 <90868232+macan0@users.noreply.github.com>
2021-09-18 23:16:03 +0200

    Update traccar-uninstall.sh

--- traccar-uninstall.sh
+++ traccar-uninstall.sh
@@ -1,1 +1,5 @@
 sudo systemctl stop traccar | sudo systemctl disable traccar | sudo rm /etc/systemd/system/traccar.service | sudo systemctl daemon-reload | sudo rm -R /opt/traccar
+
+echo "+-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
+|P||o||w||e||r||e||d| |b||y|   https://github.com/macan0    Need a custom Traccar design: https://www.fiverr.com/users/macano84
++-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+"
